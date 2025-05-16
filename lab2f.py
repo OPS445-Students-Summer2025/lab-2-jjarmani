270630df1181f12c65b6e4cf3ba7f248ffe54dd1
Sing Man Wong <smwong16@myseneca.ca>
2025-05-16 17:44:11 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Sing Man Wong
+# Author ID: 170543235
+# Date Created: 2025/05/16
+
+import sys
+
+count = int(sys.argv[1])
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
